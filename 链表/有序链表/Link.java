6303adc8483213f79b6cb16e94acf767b530ab44
pengzhangsheng <2825692566@qq.com>
2020-02-02 23:08:36 +0800

    添加有序链表Java实现
    添加有序链表Java实现

---
+++ 链表/有序链表/Link.java
@@ -0,0 +1,14 @@
+package com.company.link.firstlast;
+
+public class Link {
+    public long dData;
+    public Link next;
+
+    public Link(long dd) {
+        dData = dd;
+    }
+
+    public void displayLink() {
+        System.out.println(dData + " ");
+    }
+}
---
+++ 链表/有序链表/SortListApp.java
@@ -0,0 +1,21 @@
+package com.company.link.firstlast;
+
+public class SortListApp {
+    public static void main(String[] args) {
+        SortedList theSortedList = new SortedList();
+        theSortedList.insert(20);
+        theSortedList.insert(40);
+
+        theSortedList.displayList();
+
+        theSortedList.insert(10);
+        theSortedList.insert(30);
+        theSortedList.insert(50);
+
+        theSortedList.displayList();
+
+        theSortedList.remove();
+
+        theSortedList.displayList();
+    }
+}
